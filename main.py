f1e465ae0b381eac630e7d675de0f60c186d8e84
jgutierrezl <yogulanacio@gmail.com>
2023-12-21 13:43:28 -0500

    nuevos arreglos main

---
+++ main.py
@@ -0,0 +1,10 @@
+from funciones import *
+from fastapi import FastAPI
+import pandas as pd
+from surprise import SVD
+app = FastAPI()
+
+
+@app.get('/')
+def bienvenida():
+    return {'API de consultas a una base de datos de Steam, /docs en el link para acceder a las funciones de consulta.'}
