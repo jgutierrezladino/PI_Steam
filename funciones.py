99ff59c6bd7d7e31f78a5bdafbfc6a13500bec01
Jonathan Gutierrez <yogulanacio@gmail.com>
2023-12-26 18:51:09 -0500

    correcciones de funsiones 10%

--- funciones.py
+++ funciones.py
@@ -5,6 +5,8 @@ import gzip
 import zipfile
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.neighbors import NearestNeighbors
+from sklearn.model_selection import train_test_split
+
 
 ## cargamos los archivos
 
@@ -19,9 +21,13 @@ df_reviews = pd.read_csv('./DataSets/df_user_reviews.csv')
 
 ## Año con más horas jugadas por genero ##
 
-def PlayTimeGenre(genre):
+import pandas as pd
+
+def PlayTimeGenre(genre, percentage=0.1):
+    # Tomar un 10% de los datos de df_games
+    subset_df_games = df_games.sample(frac=percentage, random_state=42)
     # Filtrar juegos por género
-    filtered_games = df_games[df_games['genres'].str.contains(genre, case=False, na=False)]
+    filtered_games = subset_df_games[subset_df_games['genres'].str.contains(genre, case=False, na=False)]
     # Unir DataFrames
     merged_df = pd.merge(df_user, filtered_games, left_on='item_id', right_on='id')
     # Agregar horas de juego por año
@@ -30,14 +36,16 @@ def PlayTimeGenre(genre):
     max_year = grouped_df.loc[grouped_df['playtime_forever'].idxmax()]['release_date']
     # Extraer solo el año
     year_only = max_year.split('-')[0]
-    result_string = 'El año con más horas jugadas para el género ' + genre + ' es: ' + str(year_only)
+    result_string = 'El año con más horas jugadas para el género: ' + genre + ' es: ' + str(year_only)
     return result_string
 
 ## Usuario con mas horas jugadas por genero. ##
 
-def UserForGenre(genre):
+def UserForGenre(genre, percentage=0.1):
+    # Tomar un 10% de los datos de df_games
+    subset_df_games = df_games.sample(frac=percentage, random_state=42)  # 
     # Filtrar juegos por género
-    filtered_games = df_games[df_games['genres'].str.contains(genre, case=False, na=False)]
+    filtered_games = subset_df_games[subset_df_games['genres'].str.contains(genre, case=False, na=False)]
     # Unir DataFrames
     merged_df = pd.merge(df_user, filtered_games, left_on='item_id', right_on='id')
     # Convertir la columna 'release_date' a formato de fecha y extraer el año
@@ -55,9 +63,11 @@ def UserForGenre(genre):
 
 ## Top tres más recomendadas. ##
 
-def UsersRecommend(year):
-    # Convertir la columna 'release_date' a tipo datetime
-    df_games['release_date'] = pd.to_datetime(df_games['release_date'], errors='coerce')
+def UsersRecommend(year, percentage=0.1):
+    # Tomar un 10% de los datos de df_games
+    subset_df_games = df_games.sample(frac=percentage, random_state=42)
+    # Convertir la columna 'release_date' a tipo datatime
+    subset_df_games['release_date'] = pd.to_datetime(subset_df_games['release_date'], errors='coerce')
     # Filtrar las reviews para el año dado con recomendación negativa y comentarios negativos
     filtered_reviews = df_reviews[
         (df_reviews['recommend'] == True) &
@@ -73,16 +83,18 @@ def UsersRecommend(year):
 
 ## Top tres menos recomendadas. ##
 
-def UsersNotRecommend(year):
+def UsersNotRecommend(year, percentage=0.1):
+    # Tomar un 10% de los datos de df_games
+    subset_df_games = df_games.sample(frac=percentage, random_state=42)  # Puedes ajustar el valor de random_state según tus necesidades
     # Convertir la columna 'release_date' a tipo datetime
-    df_games['release_date'] = pd.to_datetime(df_games['release_date'], errors='coerce')
+    subset_df_games['release_date'] = pd.to_datetime(subset_df_games['release_date'], errors='coerce')
     # Filtrar las reviews para el año dado con recomendación negativa y comentarios negativos
     filtered_reviews = df_reviews[
         (df_reviews['recommend'] == False) &
         (df_reviews['sentiment_analiysis'] == 0) &  # 0 para comentarios negativos
-        (pd.to_datetime(df_games['release_date']).dt.year == year)]
+        (pd.to_datetime(subset_df_games['release_date']).dt.year == year)]
     # Unir con el DataFrame de juegos para obtener los nombres de los juegos
-    merged_reviews = pd.merge(filtered_reviews, df_games[['id', 'app_name']], left_on='item_id', right_on='id')
+    merged_reviews = pd.merge(filtered_reviews, subset_df_games[['id', 'app_name']], left_on='item_id', right_on='id')
     # Contar la cantidad de veces que aparece cada juego
     top_games = merged_reviews['app_name'].value_counts().head(3)
     # Convertir el resultado a un formato de lista de diccionarios
@@ -91,9 +103,11 @@ def UsersNotRecommend(year):
 
 ## Analisis de sentimiento por año.
 
-def sentiment_analysis(year):
+def sentiment_analysis(year, percentage=0.1):
+    # Tomar un 10% de los datos de df_games
+    subset_df_games = df_games.sample(frac=percentage, random_state=42)  # Puedes ajustar el valor de random_state según tus necesidades
     # Unir las reseñas con la información de los juegos
-    merged_reviews = pd.merge(df_reviews, df_games[['id', 'release_date']], left_on='item_id', right_on='id')
+    merged_reviews = pd.merge(df_reviews, subset_df_games[['id', 'release_date']], left_on='item_id', right_on='id')
     # Filtrar las reseñas para el año dado
     filtered_reviews = merged_reviews[
         (pd.to_datetime(merged_reviews['release_date'], errors='coerce').dt.year == year)]
@@ -108,16 +122,15 @@ def sentiment_analysis(year):
 
 ### Preprocesamiento de datos: para Machine-Learning
 
+# Tomar un 10% de los datos de df_games
+subset_df_games = df_games.sample(frac=0.1, random_state=42)
+
 # Vamos a procesar la columna 'genres' para que sea más fácil de trabajar y eliminar duplicados.
 
 # Crear un DataFrame con la relación entre juegos y géneros
 df_game_genres = pd.DataFrame({'id': df_games['id'], 'genres': df_games['genres'], 'app_name': df_games['app_name']})
 # Eliminar duplicados
 df_game_genres = df_game_genres.drop_duplicates(subset=['id', 'genres'])
-
-### Matriz de géneros:
-# Construir una matriz que represente la relación entre los juegos y los géneros.
-
 # Eliminar duplicados en df_game_genres
 df_game_genres = df_game_genres.drop_duplicates(subset=['app_name', 'genres'])
 # Crear la matriz de géneros
@@ -125,19 +138,18 @@ genres_matrix = df_game_genres.pivot(index='app_name', columns='genres', values=
 
 ### Similitud del coseno entre juegos:
 #Calcular la similitud del coseno entre juegos basada en la matriz de géneros.
-
 cosine_sim = cosine_similarity(genres_matrix, genres_matrix)
 
 ### Función para obtener recomendaciones de juegos:
 
-# Función para obtener recomendaciones de juegos similares
-def get_recommendations_game(game_id, cosine_sim=cosine_sim):
-    # Obtener la fila correspondiente al juego
+# División de datos
+X_train, X_test = train_test_split(genres_matrix, test_size=0.2, random_state=42)
+# Entrenamiento del modelo
+knn_model = NearestNeighbors(n_neighbors=5, metric='cosine')
+knn_model.fit(X_train)
+# Función para obtener recomendaciones usando el modelo entrenado
+def get_recommendations_knn(game_id, model=knn_model):
     game_row = genres_matrix.loc[game_id].values.reshape(1, -1)
-    # Calcular la similitud del coseno entre el juego y todos los demás juegos
-    sim_scores = cosine_similarity(game_row, genres_matrix.values)
-    # Obtener los juegos más similares (excluyendo el juego de entrada)
-    similar_games = sim_scores.argsort()[0][::-1][1:]
-    # Tomar los primeros 5 juegos recomendados
-    recommendations = genres_matrix.index[similar_games][:5].tolist()
+    _, indices = model.kneighbors(game_row)
+    recommendations = genres_matrix.index[indices[0][1:6]].tolist()
     return recommendations
