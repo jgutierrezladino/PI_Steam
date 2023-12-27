2ff798eb37477c031fa645c70d0884e10f96bf9a
Jonathan Gutierrez <yogulanacio@gmail.com>
2023-12-27 17:11:47 -0500

    cargar las funsiones

--- main.py
+++ main.py
@@ -4,6 +4,38 @@ import pandas as pd
 app = FastAPI()
 
 
+# Ruta de bienvenida
 @app.get('/')
 def bienvenida():
     return {'API de consultas a una base de datos de Steam, /docs en el link para acceder a las funciones de consulta.'}
+
+# Ruta para la función PlayTimeGenre
+@app.get('/play_time_genre/{genre}')
+def play_time_genre(genre: str):
+    result = PlayTimeGenre(genre)
+    return {'result': result}
+
+# Ruta para la función UserForGenre
+@app.get('/user_for_genre/{genre}')
+def user_for_genre(genre: str):
+    result = UserForGenre(genre)
+    return {'result': result}
+
+# Ruta para la función UsersRecommend
+@app.get('/users_recommend/{year}')
+def users_recommend(year: int):
+    result = UsersRecommend(year)
+    return {'result': result}
+
+# Ruta para la función UsersNotRecommend
+@app.get('/users_not_recommend/{year}')
+def users_not_recommend(year: int):
+    result = UsersNotRecommend(year)
+    return {'result': result}
+
+# Ruta para la función sentiment_analysis
+@app.get('/sentiment_analysis/{year}')
+def sentiment_analysis_route(year: int):
+    result = sentiment_analysis(year)
+    return {'result': result}
+
