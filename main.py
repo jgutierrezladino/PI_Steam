2c7bf89911171d8f6125ec50a68299f103ec5abe
Jonathan Gutierrez <yogulanacio@gmail.com>
2023-12-27 17:26:03 -0500

    correccion main.py

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from funciones import *
 from fastapi import FastAPI
 import pandas as pd
 app = FastAPI()
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, debug=True)
 
 
 # Ruta de bienvenida
